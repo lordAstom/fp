1ca48716990ec67759249ca3a9c547a27f5c685b
alumno <98astom@gmail.com>
2025-09-24 14:18:22 +0200

    clase 24-9-25

--- python/class.py
+++ python/class.py
@@ -5,7 +5,17 @@ esto es un bloque de texto
 '''
 comment block
 '''
-edad = 50
-sueldo = 2200,55
-apellidos = "Morales Vazquez"
-contratado = True
+def class_stuff():
+    edad = 50
+    sueldo = 2200,55
+    apellidos = "Morales Vazquez"
+    contratado = True
+    precio = 75.5
+    pvp = precio*1.21
+    print("el pvp es",pvp, end="", sep="---")
+    print(5//2)
+    print(5%2)
+    x= 0
+    x += 1
+
+class_stuff()
