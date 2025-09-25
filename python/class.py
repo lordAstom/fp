a42b83a51398d0d7663f70c29c6e4e7ba6a1ae4c
alumno <98astom@gmail.com>
2025-09-25 14:11:12 +0200

    clase25-9-25

--- python/class.py
+++ python/class.py
@@ -1,3 +1,5 @@
+import random
+
 print("Hello world") #comment
 """
 esto es un bloque de texto
@@ -18,4 +20,14 @@ def class_stuff():
     x= 0
     x += 1
 
-class_stuff()
+#class_stuff()
+"""
+nombre = input("cual es tu nombre?: ")
+print("Tu nombre es", nombre)
+edad = int(input("cuantos años tienes?:"))
+print(f"Tu nombre es {nombre} y tienes {edad} años")
+edad += 1
+"""
+dado = random.randint(a=1,b=6)
+dado2 = random.randint(1,49)
+print(dado,dado2)
